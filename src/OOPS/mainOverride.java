35a2eb7b2624ade57c1e9bcf9c881a3584fef30f
pc <pc@DESKTOP-5JVE3C3>
2023-08-07 14:04:49 +0530

    override_prac

--- src/OOPS/mainOverride.java
+++ src/OOPS/mainOverride.java
@@ -2,17 +2,19 @@ package OOPS;
 
 public class mainOverride {
 
+	
 	public static void main(String[] args) {
 		
 		System.out.println("main");
-		mainOverride o = new mainOverride();
+		mainOverride o = new mainOverride(); //object of child class
 		int []a= {1,2,3,4};
+		String []s= {"sed","cded","dsd"};
 		o.main(a);
-		o.main(0);
-		//o.main(args); - gives run time error
-		
+		o.main(3);
+		//o.main(s); // - gives run time error
 		
 	}
+	
 	
 	public static void main(int[] args) {
 		
@@ -27,11 +29,13 @@ public class mainOverride {
 
 }
 
+
 class sub extends mainOverride{
 	
 	public static void main(String[] args) {
 		
 		System.out.println("Override method");
+		main(0);
 	}
 	void print() {
 		System.out.println("print");
---
+++ src/OOPS/override_prac.java
@@ -0,0 +1,26 @@
+package OOPS;
+
+class override_prac{
+
+	public static void main(String[] args) {
+		System.out.println("main");
+		//override_prac1 o = new override_prac1();
+		String []s= {"dsfdr","sdfed"};
+		//o.main(s);
+
+	}
+
+}
+
+
+
+/*
+class override_prac1 {  //here whatever is the file name is being considered as main method.
+
+	public static void main(String[] args) {
+		
+		System.out.println("main1");
+	}
+
+}
+*/
